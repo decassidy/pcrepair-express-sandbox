bc9be8188f06300bb7bd1dda99a0514d33d2fa4a
Derrick Cassidy <decassidy@icloud.com>
2018-03-18 08:05:49 -0400

    updates 3/18/2018

---
+++ routes/contact.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+var nodemailer = require('nodemailer');
+
+/* GET contact page. */
+router.get('/', function(req, res, next) {
+  res.render('contact', {
+    title: 'Contact'
+  });
+});
+
+// Send email
+router.post('/send', function(req, res, next) {
+  var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: 'dc69@student.london.ac.uk',
+      pass: ''
+    }
+  });
+
+  var mailOptions = {
+    from: '"Derrick Cassidy" <dc69@student.london.ac.uk>',
+    to: 'londonlaw2015@icloud.com',
+    subject: 'Hello from PC Repair',
+    text: `You have a submission from... Name: ${req.body.name} Email: ${req.body.email} Message: ${req.body.message}`,
+    html: `'<p>You have a submission from...</p><ul><li> Name: ${req.body.name}</li><li> Email: ${req.body.email}</li><li> Message: ${req.body.message}</li></ul>`
+  }
+
+  transporter.sendMail(mailOptions, function(error, info) {
+    if(error) {
+      return console.log(error);
+    }
+    console.log('Message sent: '+ info.response);
+    res.redirect('/');
+  });
+
+});
+
+module.exports = router;
